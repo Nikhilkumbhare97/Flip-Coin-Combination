b23114e500b8d0ce4b5a562e4a52668048fd301d
nikhilkumbhare <nikhilk190697@gmail.com>
2021-02-13 16:01:22 +0530

    Added: To Find Singlet Combination And Store In Dictionary And Also Determine Its Percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,42 @@
 #!/bin/bash -x
 
-result=$((RANDOM%2))
+#Constants
+#Number of Coins Flipping Simultaneously
+ONE=1
+
 isHeads=1
 
-if [ $result -eq $isHeads ]
-then
-		echo Heads
-else
-		echo Tails
-fi
+declare -A coinArray
+declare -A percentage
+
+function coinFlip(){
+        for (( numOfFlip=0; numOfFlip<$1; numOfFlip++ ))
+        do
+					side=""
+                for (( coins=0; coins<$2; coins++ ))
+                do
+                        result=$((RANDOM%2))
+                        if [ $result -eq $isHeads ]
+                        then
+                                side+="H"
+                        else
+                                side+="T"
+                        fi
+                                coinArray[$side]=$((${coinArray[$side]}+1));
+                done
+                percentage $side $1
+        done
+        echo "${!percentage[@]}  : ${percentage[@]}"
+}
+
+function percentage(){
+        side=$1;
+        flip=$2;
+        percentage[$side]=$(( ${coinArray[$side]}*100/$flips ));
+}
+
+read -p "Enter Number Of Flips : " flips
+
+echo singlet=$( coinFlip $flips $ONE )
+
+
